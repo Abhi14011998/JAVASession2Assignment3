1ad37d8baecc30a5e1801bc3da58e2185dfc7bf5
Abhinav Sawant <abhinavsawantcool@gmail.com>
2017-05-26 14:08:54 +0530

    loop
    java loop

---
+++ Javasession2assignment3.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasession2assignment3;
+
+/**
+ *
+ * @author Abhinav
+ */
+public class Javasession2assignment3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        {
+		int i,j,k,l;
+		for(i=1;i<=5;i++){
+			for(j=1;j<=i;j++)
+                        {
+				System.out.print(+j);
+			}
+			System.out.println();
+			}
+		for(k=5;k>=1;k--)
+                {
+		for(l=1;l<k;l++)
+                {
+		System.out.print(+l);
+		}
+		System.out.println();
+		}
+	}
+    }
+    
+}
